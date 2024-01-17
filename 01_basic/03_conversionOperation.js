a3bcb850dd2a12095b06bf01aa1302d3a1b4c06c
FAUZAN AHMAD <110845300+khanfauzan1@users.noreply.github.com>
2024-01-17 10:09:31 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -24,5 +24,32 @@ let isLoggedIn = 1
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
 
+// **** operations **** //
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2); // 4
+// console.log(2**3); // 8
+
+let str1 ="hello"
+let str2 =" world"
+let str3= str1+str2  // hello world
+
+// console.log("1"+2); // 12
+// console.log(1+"2"); // 12
+// console.log("1"+2+2); // 122
+
+// console.log(1+2+"2"); // 32
+
+// console.log(+true); //1
+
+let num1, num2, num3
+// num1=num2=num3= 2+2;
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
