9b83d079d9d99cd1e489c15f11058ecbbce09e15
FAUZAN AHMAD <110845300+khanfauzan1@users.noreply.github.com>
2024-01-17 09:53:00 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score =" 33abc";
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);  
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 => true
+// "" => false
+// "fauzan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
